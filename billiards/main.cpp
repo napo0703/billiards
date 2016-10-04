6403fb8d2b062273c5e7c0ffdcafdccc2fd5cf8a
Naoaki Yamada <napo0703@gmail.com>
2016-10-04 20:11:43 +0900

    Initial Commit

---
+++ billiards/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  billiards
+//
+//  Created by Naoaki Yamada on 2016/10/04.
+//  Copyright © 2016年 Naoaki Yamada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
